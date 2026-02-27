ca56fb32908b954077e7d0e2a00e35319fe72d6d
khushboovlog <shivshankargupta1972@gmail.com>
2026-02-27 11:57:25 +0530

    updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,8 +4,8 @@ import express from "express";
 import { authMiddleware,authorize } from "../middleware/auth.js";
 const userRouter=express.Router();
 userRouter.get("/",showUsers)
-userRouter.post("/adduser",authMiddleware,authorize("admin"),addUser)
-userRouter.delete("/deleteuser/:id",authMiddleware,authorize("admin"),deleteUser)
+userRouter.post("/adduser",addUser)
+userRouter.delete("/deleteuser/:id",deleteUser)
 userRouter.post("/login",login)
 userRouter.patch("/:id",update)
 export default userRouter;
